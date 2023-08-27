4b4979464d223cee1318daf5ce3c2805100b5c33
Ishaant Kumar Singh <ishaant97@gmail.com>
2023-08-27 13:39:41 +0530

    Added appropriate comments for better understanding

--- 2Binary Search/main.py
+++ 2Binary Search/main.py
@@ -1,14 +1,17 @@
 import random
 
+# Create an empty list to store random numbers
 list1 = []
 
+# Generate 41 random numbers between 0 and 100 (inclusive) and store them in list1
 for i in range(0, 41):
-
     num = random.randint(0, 100)
-
     list1.append(num)
 
+# Sort the list in ascending order
 list1.sort()
+
+# Define a function for binary search
 
 
 def binarySearch(list1, target):
@@ -18,16 +21,20 @@ def binarySearch(list1, target):
     while (l <= r):
         middle = (l+r)//2
         if (list1[middle] == target):
-            return middle
+            return middle  # Target found at index 'middle'
         elif (list1[middle] > target):
-            r = middle - 1
+            r = middle - 1  # Adjust the right boundary
         elif (list1[middle] < target):
-            l = middle + 1
+            l = middle + 1  # Adjust the left boundary
         else:
-            return -1
+            return -1  # Target not found
 
 
+# Define the target value to search for
 target = 10
+
+# Call the binarySearch function to find the target in the sorted list
 result = binarySearch(list1, target)
 
+# Print the result of the search
 print(result)

9d68be0fc7694a24824e92d0bef50a416930d434
Ishaant Kumar Singh <ishaant97@gmail.com>
2023-10-09 08:45:25 +0530

    Added comments for better understanding

--- 6QuizApp/main.py
+++ 6QuizApp/main.py
@@ -7,6 +7,7 @@ class QuizApp:
         self.root = root
         self.root.title("Python Quiz")
 
+        # Load questions from a JSON file
         self.questions = self.load_questions("questions.json")
 
         self.current_question = 0
@@ -17,11 +18,14 @@ class QuizApp:
         self.timer = 30
         self.timer_id = None
 
+        # Enable fullscreen mode
         root.attributes('-fullscreen', True)
 
+        # Create the label to display the question
         self.question_label = tk.Label(root, text="", font=("Arial", 14))
         self.question_label.pack()
 
+        # Create radio buttons for answer options
         self.option_buttons = []
         for i in range(4):
             option_button = tk.Radiobutton(
@@ -29,6 +33,7 @@ class QuizApp:
             self.option_buttons.append(option_button)
             option_button.pack()
 
+        # Create "Next" and "Finish Test" buttons
         self.next_button = tk.Button(
             root, text="Next", command=self.check_answer, fg="black")
         self.next_button.pack()
@@ -37,22 +42,27 @@ class QuizApp:
             root, text="Finish Test", command=self.finish_test, fg="black")
         self.finish_button.pack()
 
+        # Create label to display the score
         self.score_label = tk.Label(root, text="")
         self.score_label.pack()
 
+        # Create label to display the timer
         self.timer_label = tk.Label(
             root, text="", font=("Arial", 14), fg="black")
         self.timer_label.pack(side=tk.RIGHT)
 
+        # Load the first question and start the timer
         self.load_next_question()
         self.update_timer()
 
     def load_questions(self, filename):
+        # Load questions from a JSON file
         with open(filename, 'r') as file:
             questions = json.load(file)
         return questions
 
     def load_next_question(self):
+        # Load the next question and update the UI
         if self.current_question < len(self.questions):
             question_data = self.questions[self.current_question]
             question_text = question_data["question"]
@@ -64,9 +74,11 @@ class QuizApp:
                 option_button.config(text=options[i])
             self.selected_option.set(None)
         else:
+            # All questions have been answered, submit the quiz
             self.submit_quiz()
 
     def check_answer(self):
+        # Check the selected answer and update the score
         if self.selected_option.get():
             selected_answer = int(self.selected_option.get())
             if selected_answer == self.correct_answer:
@@ -77,6 +89,7 @@ class QuizApp:
             self.load_next_question()
 
     def submit_quiz(self):
+        # Display the score and disable buttons
         self.show_score_and_wrong_answers()
         self.next_button.config(state="disabled")
         self.finish_button.config(state="disabled")
@@ -84,6 +97,7 @@ class QuizApp:
         self.stop_timer()
 
     def show_score_and_wrong_answers(self):
+        # Display the score and list of wrong answers
         score_text = f"Your score: {self.score}/{len(self.questions)}"
         wrong_answers_text = ""
         if self.wrong_answers:
@@ -94,17 +108,20 @@ class QuizApp:
                 correct_answer = question_data["options"][question_data["correct_answer"] - 1]
                 wrong_answers_text += f"Q{question_number}: {question_text}\nCorrect Answer: {correct_answer}\n"
 
+        # Create a score window to display the results
         self.score_window = tk.Toplevel(self.root)
         self.score_window.title("Quiz Score")
         score_label = tk.Label(
             self.score_window, text=score_text + wrong_answers_text, font=("Arial", 14))
         score_label.pack()
 
+        # Add a button to retake the quiz
         retry_button = tk.Button(
             self.score_window, text="Retake Quiz", command=self.restart_quiz)
         retry_button.pack()
 
     def restart_quiz(self):
+        # Reset the quiz to its initial state
         self.current_question = 0
         self.score = 0
         self.wrong_answers = []
@@ -119,10 +136,12 @@ class QuizApp:
             self.score_window.withdraw()
 
     def finish_test(self):
+        # Finish the test (same as submitting)
         self.submit_quiz()
 
     def update_timer(self):
         if self.timer > 0:
+            # Update the timer label and button colors
             timer_color = "red" if self.timer < 10 else "black"
             self.timer_label.config(
                 text=f"Time Left: {self.timer} sec", fg=timer_color)
@@ -136,10 +155,12 @@ class QuizApp:
             self.timer -= 1
             self.timer_id = self.root.after(1000, self.update_timer)
         elif self.timer == 0:
+            # Timer has reached zero, submit the quiz
             self.submit_quiz()
             self.timer = -1
 
     def stop_timer(self):
+        # Stop the timer (cancel timer event)
         if self.timer_id:
             self.root.after_cancel(self.timer_id)
 
